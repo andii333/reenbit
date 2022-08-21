c5f177fdbd94d8585934d73df697a6aa1821cd98
Andrii Semeriak <saz9002@gmail.com>
2022-08-21 22:38:34 +0300

    last commit
    Signed-off-by: Andrii Semeriak <saz9002@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Message } from './contact-chat/messages';
+import { FunctionsService } from './functions.service';
 import { GoogleApiService } from './google-api.service';
 
 @Component({
@@ -10,6 +12,20 @@ export class AppComponent {
   title = 'reenbit';
 
   constructor(
+    public functions: FunctionsService,
     // private readonly google: GoogleApiService
-    ) {}
+    ) {
+      this.addLocalstorage()
+    }
+
+    addLocalstorage(){
+      const messages:Message [] = [];
+      messages.push(new Message('Hello',new Date(), 'dark'))
+      messages.push(new Message('Hi',new Date(), 'white'))
+      messages.push(new Message('How are You?',new Date(), 'dark'))
+      messages.push(new Message('I am good',new Date(), 'white'))
+      this.functions.toLocalStorage('Alice Freeman', messages)
+      this.functions.toLocalStorage('Iniesta', messages)
+      this.functions.toLocalStorage('Roberto', messages)
+    }
 }
